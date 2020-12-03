b39caad5cde694d21a2193cf8063115371544252
Flavius <flaviusatila@gmail.com>
2020-12-02 21:19:27 -0300

    Problema Na transection

--- assets/dist/js/cadastro.js
+++ assets/dist/js/cadastro.js
@@ -1,21 +1,50 @@
-var db = openDatabase("MeuBanco","2.0", "Mybase", 4048);
+var db = openDatabase("MeuBanco","2.0", "Mybase", 2 * 1024 * 1024);
 var cont;
 criar = async () => {
         await db.transaction(function(levantarBanco){
             levantarBanco.executeSql("CREATE TABLE users (ID PRIMARY KEY, email TEXT, senha TEXT)");
 });
 }
-// setAlert = (message) => { 
-//     var ele =document.createElement('div') 
-//     ele.innerHTML =`<div class="alert alert-danger" role="alert">${message}</div>`
-//     document.body.appendChild(ele);
-// }
+
+
+ salvar = async () =>{
+    var email = document.getElementById('cadastroEmail').value;
+    var teste = false;
+    await db.transaction((find) =>{ 
+     find.executeSql("SELECT * FROM users WHERE email = ?", [email],
+    (find,results) => {
+     var len = results.rows.length;
+    console.log('len', len);
+    if (len > 0) {
+        alert('Usuario já Cadastrado');
+        teste = true;
+        // redirectCadastro();
+    }else{
+        alert("Diferente");
+        // salvardb();
+        teste = false;
+    
+    }});
+});
+    if(teste){
+        // redirectCadastro();
+        alert("Algum outro Alert");
+    } else {
+        // teste = false;
+       await salvardb();
+       alert("no else tbm");
+    }
+    // await Promise.all(veremos) 
+//    await db.transaction(function(find){ metodo2(find)});
+}
+
 salvardb = async () => {
     var email = document.getElementById('cadastroEmail').value;
     var senha = document.getElementById('cadastroSenha').value;
-    await db.transaction(async (salvarUser) => {
-        salvarUser.executeSql("INSERT INTO users (email, senha) VALUES (?,?)",[email,senha]);
-        alert("Novo usuario Cadastrado com sucesso")
+    await  db.transaction(async (salvarUser) => {
+        await salvarUser.executeSql("INSERT INTO users (email, senha) VALUES (?,?)",[email,senha]);
+        alert("Novo usuario Cadastrado com sucesso");
+       await redirectCadastro();
     });
 }
 
@@ -25,7 +54,7 @@ salvardb = async () => {
 
         await db.transaction(async (find) => {
             await find.executeSql("SELECT * FROM users WHERE email = ? AND senha = ?", [email, senha],
-           (find, results) => {
+           (find,results) => {
             var len = results.rows.length;
             console.log('len', len);
             if (len > 0) {
@@ -38,22 +67,25 @@ salvardb = async () => {
 
     }   
 
-    salvar = () =>{
-        var email = document.getElementById('cadastroEmail').value;
-        db.transaction(async (find) => {
-           await find.executeSql("SELECT * FROM users WHERE email = ?", [email],
-           (find, results) => {
-            var len = results.rows.length;
-            console.log('len', len);
-            if (len > 0) {
-                alert('Usuario já Cadastrado');
-            } else {
-               salvardb();
-            }
-           });
-        });
-    }
+ metodo2 = (find) =>{
+    // var email = document.getElementById('cadastroEmail').value;
+    // find.executeSql("SELECT * FROM users WHERE email = ?", [email],
+    // (find,results) => metodo(find,results));
+ }
 
-    redirect = () => {window.location.href = "index.html"}
+ metodo = (find,results) =>{
+    var len = results.rows.length;
+    console.log('len', len);
+    if (len > 0) {
+        alert('Usuario já Cadastrado');
+        redirectCadastro();
+    } else {
+      salvardb();
+    }
+ }
+  
+
+    redirectIndex = () => {window.location.href = "index.html"}
+    redirectCadastro = () => {window.location.href = "cadastro.html"}
 
     
---
+++ assets/dist/js/cadastro2.js
@@ -0,0 +1,41 @@
+var db = openDatabase("MeuBanco","2.0", "Mybase", 4048);
+criar = async () => {
+        await db.transaction(function(levantarBanco){
+            levantarBanco.executeSql("CREATE TABLE users (ID PRIMARY KEY, email TEXT, senha TEXT)");
+});
+}
+
+salvar = async () => {
+    var email = document.getElementById('cadastroEmail').value;
+    var senha = document.getElementById('cadastroSenha').value;
+ 
+    await db.transaction(async (salvarUser) => {
+        await salvarUser.executeSql("INSERT INTO users (email, senha) VALUES (?,?)",[email,senha]);
+    });
+    await redirect();
+    alert('Usuario Cadastrado');
+}
+
+    findBy = async () => {
+        var email = document.getElementById('inputEmail').value;
+        var senha = document.getElementById('inputPassword').value;
+        await db.transaction(async (find) => {
+            await find.executeSql("SELECT * FROM users WHERE email = ? AND senha = ?", [email, senha],
+           (find, results) => {
+            var len = results.rows.length;
+            console.log('len', len);
+            if (len > 0) {
+                alert('Usuario Cadastrado');
+            } else {
+                alert('Usuario nao Cadastrado');
+            }
+           });
+        });
+
+    }   
+
+    redirect = () => {window.location.href = 'index.html'}
+
+
+
+    redirectCadastro = () => {window.location.href = "cadastro.html"} 
