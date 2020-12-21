d1421d9fde7bb993e2655f81dee1448bc22362e9
Flavius <flaviusatila@gmail.com>
2020-12-20 23:54:24 -0300

    Commit initial

---
+++ src/main/java/eVacina/evacina/EvacinaApplication.java
@@ -0,0 +1,13 @@
+package eVacina.evacina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvacinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvacinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eVacina/evacina/EvacinaApplicationTests.java
@@ -0,0 +1,13 @@
+package eVacina.evacina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvacinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
